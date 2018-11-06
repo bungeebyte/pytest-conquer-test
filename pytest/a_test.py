6f5987bb3f50a1941b95518b866dbf01496b3cac
Stephan Behnke <git@stephanbehnke.de>
2018-11-05 19:21:25 -0800

    initial commit

---
+++ pytest/a_test.py
@@ -0,0 +1,5 @@
+import time
+
+
+def test():
+    time.sleep(1)
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='maketestsgofaster-test',
+    version='1.0.0',
+    license='MIT'
+)
